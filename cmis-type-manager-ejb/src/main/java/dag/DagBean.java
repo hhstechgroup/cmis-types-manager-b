1e4c72e50ee9485b20f7ea2fac8a1abef18216fb
Alex <qnexdev@gmail.com>
2013-11-18 15:53:14 +0200

    chemistry is work

--- cmis-type-manager-ejb/src/main/java/dag/DagBean.java
+++ cmis-type-manager-ejb/src/main/java/dag/DagBean.java
@@ -1,6 +1,15 @@
 package dag;
 
+import org.apache.chemistry.opencmis.client.api.*;
+import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
+import org.apache.chemistry.opencmis.commons.SessionParameter;
+import org.apache.chemistry.opencmis.commons.enums.BindingType;
+
 import javax.ejb.Stateless;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,7 +25,25 @@ public class DagBean implements Dag {
 
     @Override
     public String sayHey() {
-        return "EJB says hey. Via EJB: ";
+        String atomUrl = "http://winctrl-tdl6ti6:8080/chemistry-opencmis-server-inmemory-0.10.0/atom11";
+        Map<String, String> parameter = new HashMap<String, String>();
+        List<String> folders = new ArrayList<String>();
+        parameter.put(SessionParameter.USER, "");
+        parameter.put(SessionParameter.PASSWORD, "");
+        parameter.put(SessionParameter.ATOMPUB_URL, atomUrl);
+        parameter.put(SessionParameter.BINDING_TYPE, BindingType.ATOMPUB.value());
+        parameter.put(SessionParameter.REPOSITORY_ID, "A1");
+        SessionFactory sessionFactory = SessionFactoryImpl.newInstance();
+        Repository repository = sessionFactory.getRepositories(parameter).get(0);
+        Session session = sessionFactory.createSession(parameter);
+        if (session != null) {
+            Folder root = session.getRootFolder();
+            ItemIterable<CmisObject> children = root.getChildren();
+            for (CmisObject child : children) {
+                folders.add(child.getName());
+            }
+        }
+        return folders.get(0);
     }
 
 }
