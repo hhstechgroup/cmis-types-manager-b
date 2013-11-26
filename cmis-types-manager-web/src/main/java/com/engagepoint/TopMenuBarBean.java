0d588e2a7b762d19707f69767bd316b53fc10224
Alex <alex.denisenko@engagepoint.com>
2013-11-26 17:18:01 +0200

    delete class

--- cmis-types-manager-web/src/main/java/com/engagepoint/TopMenuBarBean.java
+++ cmis-types-manager-web/src/main/java/com/engagepoint/TopMenuBarBean.java
@@ -11,11 +11,9 @@ import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 
 /**
- * Created with IntelliJ IDEA.
  * User: sergii.serba
  * Date: 11/18/13
  * Time: 6:06 PM
- * To change this template use File | Settings | File Templates.
  */
 @ManagedBean(name = "topMenuBar")
 @RequestScoped
