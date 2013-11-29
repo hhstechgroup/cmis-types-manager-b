d7c1c247599da55e9b4aade88bb8580a349c2628
victor.klymenko <victor.klymenko@engagepoint.com>
2013-11-29 12:28:45 +0200

    TB-48

--- cmis-types-manager-ejb/src/main/java/com/engagepoint/CMISConnectException.java
+++ cmis-types-manager-ejb/src/main/java/com/engagepoint/CMISConnectException.java
@@ -1,11 +1,9 @@
 package com.engagepoint;
 
 /**
- * Created with IntelliJ IDEA.
  * User: michael.vakulik
  * Date: 11/27/13
  * Time: 6:59 PM
- * To change this template use File | Settings | File Templates.
  */
 public class CmisConnectException extends Exception {
     public CmisConnectException(String message) {
