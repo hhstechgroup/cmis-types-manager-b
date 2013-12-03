76e5b0e1d0b7eb0ee44073c5e8650abde9c7f15d
stanislav-skrebtsov-engagepoint <stanislav.skrebtsov@engagepoint.com>
2013-12-03 18:26:17 +0200

    Merge branch 'develop' of https://github.com/engagepoint/cmis-types-manager-b into develop
    Conflicts:
    	cmis-types-manager-web/src/main/webapp/dashboard/index.xhtml
    	cmis-types-manager-web/src/main/webapp/dashboard/type.xhtml
    
    Bad commit

--- cmis-types-manager-web/src/main/java/com/engagepoint/view/NavigationBean.java
+++ cmis-types-manager-web/src/main/java/com/engagepoint/view/NavigationBean.java
@@ -2,6 +2,7 @@ package com.engagepoint.view;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
 
 /**
@@ -13,9 +14,11 @@ import java.io.Serializable;
 @SessionScoped
 public class NavigationBean implements Serializable {
     private static final String TO_LOGIN = "/login?faces-redirect=true";
-    private static final String TO_MAIN_PAGE = "/dashboard/index?faces-redirect=true";
-    private static final String TO_MAIN_PAGE2 = "index?faces-redirect=true";
-    private static final String TO_CREATE_TYPE = "type?faces-redirect=true";
+    private static final String TO_MAIN_PAGE_D = "/dashboard/index?faces-redirect=true";
+    private static final String TO_MAIN_PAGE = "index?faces-redirect=true";
+    private static final String TO_CREATE_TYPE = "create?faces-redirect=true";
+    private static final String TO_UPDATE_TYPE = "type?faces-redirect=true";
+    private static final String TO_VIEW_TYPE = "type?faces-redirect=true";
 
     public String toLogin(){
         return TO_LOGIN;
@@ -25,8 +28,8 @@ public class NavigationBean implements Serializable {
         return TO_MAIN_PAGE;
     }
 
-    public String toMainPage2(){
-        return TO_MAIN_PAGE2;
+    public String getToMainPageD(){
+        return TO_MAIN_PAGE_D;
     }
 
     public String toCreateType(){
@@ -34,10 +37,10 @@ public class NavigationBean implements Serializable {
     }
 
     public String toUpdateType(){
-        return TO_CREATE_TYPE;
+        return TO_UPDATE_TYPE;
     }
 
     public String toViewType(){
-        return TO_CREATE_TYPE;
+        return TO_VIEW_TYPE;
     }
 }
