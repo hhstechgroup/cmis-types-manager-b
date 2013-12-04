65207c39861294697a2412196b0b58b7350bc6d9
stanislav-skrebtsov-engagepoint <stanislav.skrebtsov@engagepoint.com>
2013-12-04 15:28:40 +0200

    Fixed navigation issue in doLogin method

--- cmis-types-manager-web/src/main/java/com/engagepoint/view/LoginBean.java
+++ cmis-types-manager-web/src/main/java/com/engagepoint/view/LoginBean.java
@@ -48,8 +48,8 @@ public class LoginBean implements Serializable {
             }
         } catch (CmisConnectException e) {
             FacesContext.getCurrentInstance().addMessage("exceptions", new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
+            return navigationBean.toLogin();
         }
-        return "";
     }
 
     public String logout() {
