e316a20850c91218fad3ffb17fc98b42ce023cbd
VyacheslavPolulyakh <vyacheslav.polulyakh@engagepoint.com>
2013-12-19 14:58:44 +0200

    deleted rebel.xml

--- cmis-types-manager-web/src/main/java/com/engagepoint/filters/LoginFilter.java
+++ cmis-types-manager-web/src/main/java/com/engagepoint/filters/LoginFilter.java
@@ -8,7 +8,6 @@ import com.engagepoint.view.LoginBean;
 import org.apache.commons.lang.StringUtils;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
