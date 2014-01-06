ce47243e66a591a8c967fa2603fd93f70cb1e718
Alex <qnexdev@gmail.com>
2014-01-06 12:54:47 +0200

    Refactoring code

--- cmis-types-manager-ejb/src/test/java/com/engagepoint/ejb/ServiceTest.java
+++ cmis-types-manager-ejb/src/test/java/com/engagepoint/ejb/ServiceTest.java
@@ -5,11 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.List;
-
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.*;
-
 /**
  * User: AlexDenisenko
  * Date: 05.01.14
@@ -24,35 +19,7 @@ public class ServiceTest {
 
     @Test
     public void testCreateType() throws Exception {
-        //mock creation
-        List mockedList = mock(List.class);
 
-        //using mock
-        mockedList.add("once");
-
-        mockedList.add("twice");
-        mockedList.add("twice");
-
-        mockedList.add("three times");
-        mockedList.add("three times");
-        mockedList.add("three times");
-
-        //following two verifications work exactly the same - times(1) is used by default
-        verify(mockedList).add("once");
-        verify(mockedList, times(1)).add("once");
-        when(mockedList.get(anyInt())).thenReturn("element");
-
-        //exact number of invocations verification
-        verify(mockedList, times(2)).add("twice");
-        verify(mockedList, times(3)).add("three times");
-
-        //verification using never(). never() is an alias to times(0)
-        verify(mockedList, never()).add("never happened");
-
-        //verification using atLeast()/atMost()
-        verify(mockedList, atLeastOnce()).add("three times");
-//        verify(mockedList, atLeast(2)).add("five times");
-        verify(mockedList, atMost(5)).add("three times");
     }
 
     @Test
