bcad6e71a3a81b6f2147ad8ebea3c02653f54bd5
Michael.Vakulik <michael.vakulik@engagepoint.com>
2014-01-08 14:28:02 +0200

    Junit Test for CustomJSONConvector

--- cmis-types-manager-ejb/src/test/java/com/engagepoint/util/CustomJSONConverterTest.java
+++ cmis-types-manager-ejb/src/test/java/com/engagepoint/util/CustomJSONConverterTest.java
@@ -1,9 +1,50 @@
 package com.engagepoint.util;
 
+import com.engagepoint.ejb.CmisConnection;
+import com.engagepoint.pojo.Type;
+import com.engagepoint.pojo.UserInfo;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.client.api.SessionFactory;
+import org.apache.chemistry.opencmis.commons.definitions.TypeDefinition;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import sun.net.www.content.text.plain;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+
 /**
  * User: victor.klymenko
  * Date: 1/8/14
  * Time: 1:29 PM
  */
 public class CustomJSONConverterTest {
+    public Type type;
+    public List<TypeDefinition> typeDefinitionList;
+    public Map<String,Object> map;
+
+    @Before
+    public void beforeRun() {
+    }
+
+    @Test
+    public void testConvertTypeDefinitions(){
+        Assert.assertNotNull(CustomJSONConverter.convertTypeDefinitions(null));
+        Assert.assertNotNull(CustomJSONConverter.convertTypeDefinitions(map));
+    }
+    @Test
+    public void testConvert(){
+
+    }
+    @Test
+    public void testWriteTypeDefinitions(){
+
+    }
+    @Test
+    public void testWriteChildrenType(){
+
+    }
 }
